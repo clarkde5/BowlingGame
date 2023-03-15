e97aa9267dbd9720fb85f4f1b76b6471e7b3b701
Derrick Clark <Derrick.Clark@autozone.com>
2023-03-15 09:33:09 -0400

    Initial commit

---
+++ BowlingGame.Tests/GameTests.cs
@@ -0,0 +1,24 @@
+namespace BowlingGame.Tests;
+
+public class GameTests
+{
+    /* 
+    Kata: [Bowling Game Kata](https://kata-log.rocks/bowling-game-kata)
+
+    Class: Game
+
+    Description:
+        Write a class Game that has two methods
+        
+        1. void roll(int) is called each time the player rolls a ball. The argument is the number of pins knocked down.
+        2. int score() returns the total score for that game.
+        
+    Test Cases:
+        TestGutterGame
+        TestSinglePinGame
+        TestOneSpare
+        TestOneStrike
+        TestPerfectGame
+    */
+
+}
